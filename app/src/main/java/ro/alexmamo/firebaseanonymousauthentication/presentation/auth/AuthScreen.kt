5474b9aa6370f7539677e0f4a85540bac2a26392
alexm <alexmamo@gmail.com>
2021-12-09 09:52:30 +0200

    Minor fixes and improvements.

--- app/src/main/java/ro/alexmamo/firebaseanonymousauthentication/presentation/auth/AuthScreen.kt
+++ app/src/main/java/ro/alexmamo/firebaseanonymousauthentication/presentation/auth/AuthScreen.kt
@@ -1,14 +1,9 @@
 package ro.alexmamo.firebaseanonymousauthentication.presentation.auth
 
 import android.util.Log
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import kotlinx.coroutines.InternalCoroutinesApi
@@ -16,6 +11,7 @@ import ro.alexmamo.firebaseanonymousauthentication.core.Constants.TAG
 import ro.alexmamo.firebaseanonymousauthentication.domain.model.Response.*
 import ro.alexmamo.firebaseanonymousauthentication.presentation.auth.components.AuthContent
 import ro.alexmamo.firebaseanonymousauthentication.presentation.auth.components.AuthTopBar
+import ro.alexmamo.firebaseanonymousauthentication.presentation.components.ProgressBar
 import ro.alexmamo.firebaseanonymousauthentication.presentation.util.Screen.ProfileScreen
 
 @Composable
@@ -39,13 +35,7 @@ fun AuthScreen(
             AuthContent()
 
             when(val response = authViewModel.signInState.value) {
-                is Loading -> Box(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator()
-                }
+                is Loading -> ProgressBar()
                 is Success -> {
                     if (response.data) {
                         navController.navigate(ProfileScreen.route)
--- app/src/main/java/ro/alexmamo/firebaseanonymousauthentication/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/ro/alexmamo/firebaseanonymousauthentication/presentation/profile/ProfileScreen.kt
@@ -1,18 +1,15 @@
 package ro.alexmamo.firebaseanonymousauthentication.presentation.profile
 
 import android.util.Log
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import kotlinx.coroutines.InternalCoroutinesApi
 import ro.alexmamo.firebaseanonymousauthentication.core.Constants.TAG
 import ro.alexmamo.firebaseanonymousauthentication.domain.model.Response.*
+import ro.alexmamo.firebaseanonymousauthentication.presentation.components.ProgressBar
 import ro.alexmamo.firebaseanonymousauthentication.presentation.profile.components.ProfileContent
 import ro.alexmamo.firebaseanonymousauthentication.presentation.profile.components.ProfileTopBar
 import ro.alexmamo.firebaseanonymousauthentication.presentation.util.Screen.AuthScreen
@@ -23,6 +20,10 @@ fun ProfileScreen(
     navController: NavController,
     profileViewModel: ProfileViewModel = hiltViewModel()
 ) {
+    LaunchedEffect(Unit) {
+        profileViewModel.getAuthState()
+    }
+
     Scaffold(
         topBar = {
             ProfileTopBar()
@@ -30,14 +31,8 @@ fun ProfileScreen(
     ) {
         ProfileContent()
 
-        profileViewModel.getAuthState()
         when(val response = profileViewModel.isUserSignedOutState.value) {
-            is Loading -> Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                CircularProgressIndicator()
-            }
+            is Loading -> ProgressBar()
             is Success -> {
                 if (response.data) {
                     navController.popBackStack()
