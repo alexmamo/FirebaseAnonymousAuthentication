2ec53090625bbbeabbb0a5307ee0547daf1674f2
Alex Mamo <alexmamo@gmail.com>
2022-05-18 09:40:49 +0300

    Minor fixes and improvements.

--- app/src/main/java/ro/alexmamo/firebaseanonymousauthentication/presentation/MainActivity.kt
+++ app/src/main/java/ro/alexmamo/firebaseanonymousauthentication/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.navigation.NavHostController
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ro.alexmamo.firebaseanonymousauthentication.presentation.auth.AuthViewModel
@@ -14,19 +15,26 @@ import ro.alexmamo.firebaseanonymousauthentication.presentation.navigation.Scree
 @AndroidEntryPoint
 @ExperimentalAnimationApi
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavHostController
     private val viewModel by viewModels<AuthViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel.getAuthState()
         setContent {
-            val navController = rememberAnimatedNavController()
+            navController = rememberAnimatedNavController()
             NavGraph(
                 navController = navController
             )
-            if(viewModel.isUserAuthenticated) {
-                navController.navigate(ProfileScreen.route)
-            }
+            checkAuthStatus()
+            getAuthState()
         }
     }
+
+    private fun checkAuthStatus() {
+        if(viewModel.isUserAuthenticated) {
+            navController.navigate(ProfileScreen.route)
+        }
+    }
+
+    private fun getAuthState() = viewModel.getAuthState()
 }
